b32c7eed9a5df800a1ec99e9624f7d8025af9332
Egor <bronnikov.40@mail.ru>
2021-07-31 21:12:45 +0300

    Add new functions, update old and update `README.md`
    Update function `Scraper.get_follower_list`, create function `Scraper.get_likes_list` which gives list of usernames who liked post and add function `Scraper.end` which end the session

--- README.md
+++ README.md
@@ -1,25 +1,43 @@
-# :selfie: Instagram Engagement Data
+<h1><img alt="instagram-logo" src="./assets/logo.webp" width=30> Instagram Engagement Data</h1>
 
 This repository is an order from a freelance.
 
 ### Task
 I have one instagram profile with 2 posts on it that I need to find out of each like if they follow the user or not. So for example if the profile has 100k followers and 50k likes on one of their posts, I need to find out how many of those 50k likes follow the profile. If you can write a script where I can easily swap profile usernames if I want to test another instagram profile for the same data extraction that would be great. Please write the script in python if possible. Expected output would be the script code and a csv file with a column for the username "@" of the "likes" on each post, a column with a 1/0 or a Yes/No if that username "@" that liked the post also follows the profile.
 
+### Documentation
+`class Scraper(username: str, password: str)` — main class that takes all the information from Instagram
+
+`def Scraper.authentication() -> None` — function with which you can log into Instagram
+
+`def Scraper.get_follower_list(profile: str) -> List[str]` — function that creates a list of followers
+
+`def Scraper.get_likes_list(post: str) -> List[str]` — function that creates a list of users who have liked the post
+
+`def Scraper.end() -> None` — function that exits the `webdriver`
+
 ### Example
 ```python
 from scraper import Scraper
+from pprint import pprint
 
 
 __USERNAME = "username"     # Your `username` from Instagram account
 __PASSWORD = "password"     # Your `password` from Instagram account
-profile_name = "profile"    # Instagram profile that interests you, for example: 'endygamedev_' 
+profile_name = "profile"    # Instagram profile that interests you, for example: 'endygamedev_'
+post_link = "post_url"      # Instagram post that insterest you, for example: 'https://www.instagram.com/p/CRmQr4yrBz0/'
+
 
 scraper = Scraper(__USERNAME, __PASSWORD)
 scraper.authentication()
-follower_list = scraper.get_follower_list(profile_name)
 
-print(len(follower_list))
-print(follower_list)
+follower_list = scraper.get_follower_list(profile_name)
+pprint({len(follower_list): follower_list})
+
+likes_list = scraper.get_likes_list(post_link)
+pprint({len(likes_list): likes_list})
+
+scraper.end()
 ```
 
 ### Dependencies
--- scraper.py
+++ scraper.py
@@ -4,12 +4,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from pprint import pprint
+from typing import List
 import time
 
 # Constants and variables
-__USERNAME = "username"     # Your `username` from Instagram account
-__PASSWORD = "password"     # Your `password` from Instagram account
-profile_name = "profile"    # Instagram profile that interests you, for example: 'endygamedev_'
+__USERNAME = "username"         # Your `username` from Instagram account
+__PASSWORD = "password"         # Your `password` from Instagram account
+profile_name = "profile_name"   # Instagram profile that interests you, for example: 'endygamedev_'
+post_link = "post_url"          # Instagram post that insterest you, for example: 'https://www.instagram.com/p/CRmQr4yrBz0/'
 
 
 class Scraper:
@@ -21,33 +24,35 @@ class Scraper:
             :type username: str
             :param password: Instagram `password` for authentication
             :type password: str
-            :return: Creates a `webdriver` and initializes variables
-            :returns: None
+            :returns: Creates a `webdriver` and initializes variables
+            :rtype: None
         """
-        self.driver = webdriver.Firefox()
+        self._driver = webdriver.Firefox()
+        self._start = time.time()
 
         self._username = username
         self._password = password
 
+        self.__TIME_SLEEP = 3
         self.__TIME_SLEEP_AUTH = 5
-        self.__TIME_SLEEP_FOLLOWERS = 3
 
     def authentication(self) -> None:
         """
             **Description:** Instagram authentication function
 
             :returns: None
+            :rtype: None
         """
-        self.driver.get("https://www.instagram.com")
+        self._driver.get("https://www.instagram.com")
         time.sleep(self.__TIME_SLEEP_AUTH)
 
         # Typing `username` in authentication system
-        action = self.driver.find_element_by_name("username")
+        action = self._driver.find_element_by_name("username")
         action.clear()
         action.send_keys(self._username)
 
         # Typing `password` in authentication system
-        action = self.driver.find_element_by_name("password")
+        action = self._driver.find_element_by_name("password")
         action.clear()
         action.send_keys(self._password)
 
@@ -55,46 +60,103 @@ class Scraper:
         action.send_keys(Keys.RETURN)
         time.sleep(self.__TIME_SLEEP_AUTH)
 
-    def get_follower_list(self, profile: str) -> list:
+    def get_follower_list(self, profile: str) -> List[str]:
         """
             **Description:** Takes the `user's nickname` and gives a list of usernames that are followed to this user
 
             :param profile: username on instagram
             :type profile: str
-            :return: List of instagram profile followers - `profile`
-            :rtype: list
+            :returns: List of instagram profile followers - `profile`
+            :rtype: List[str]
         """
-        wait = WebDriverWait(self.driver, 10)
-        self.driver.get("https://www.instagram.com/" + profile)
-        time.sleep(self.__TIME_SLEEP_FOLLOWERS)
+        wait = WebDriverWait(self._driver, 10)
+        self._driver.get("https://www.instagram.com/" + profile)
+        time.sleep(self.__TIME_SLEEP)
 
         # Get the number of followers
         followers_count = int(
-            self.driver.find_element_by_xpath('/html/body/div[1]/section/main/div/header/section/ul/li[2]/a/span').text)
+            self._driver.find_element_by_xpath(
+                '/html/body/div[1]/section/main/div/header/section/ul/li[2]/a/span').text)
 
         # Click on the `followers button`
         followers_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a[href*='followers']")))
         followers_button.click()
-        time.sleep(self.__TIME_SLEEP_FOLLOWERS)
+        time.sleep(self.__TIME_SLEEP)
         dialogue_box = wait.until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, "body > div[role='presentation'] > div > div > div.isgrP")))
 
         # Scroll the list of followers
-        for _ in range(0, followers_count // 6 + 1):
-            self.driver.execute_script('arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;',
-                                       dialogue_box)
-            time.sleep(self.__TIME_SLEEP_FOLLOWERS)
+        for _ in range(0, followers_count // 6):
+            self._driver.execute_script("arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;",
+                                        dialogue_box)
+            time.sleep(self.__TIME_SLEEP)
 
         # Get the followers list
-        html_list = self.driver.find_element_by_xpath("/html/body/div[5]/div/div/div[2]/ul")
-        followers = [html_list.find_element_by_xpath(f"//li[{i}]/div/div[1]/div[2]/div/span/a").get_attribute('title')
-                     for i in range(1, followers_count + 1)]
-        self.driver.close()  # TODO: Remove this when the list of likes is created
+        html_list = self._driver.find_element_by_xpath("/html/body/div[5]/div/div/div[2]/ul")
+        elements_count = len(html_list.find_elements_by_xpath("./div/*"))
+        followers = [html_list.find_element_by_xpath(f"//div/li[{i}]/div/div[1]/div[2]/div/span/a").get_attribute('title')
+                     for i in range(1, elements_count + 1)]
         return followers
 
+    def get_likes_list(self, post: str) -> List[str]:
+        """
+            **Description:** Takes the `URL of the post` and gives a list of usernames that are liked this post
 
-scraper = Scraper(__USERNAME, __PASSWORD)
-scraper.authentication()
-follower_list = scraper.get_follower_list(profile_name)
-print(len(follower_list))
-print(follower_list)
+            :param post: URL to the Instagram post that interests you
+            :type post: str
+            :return: List of instagram usernames who liked the `post`
+            :rtype: List[str]
+        """
+        wait = WebDriverWait(self._driver, 10)
+        self._driver.get(post)
+        time.sleep(self.__TIME_SLEEP)
+
+        # Get the number of likes
+        likes_count = int(
+            self._driver.find_element_by_xpath(
+                "/html/body/div[1]/section/main/div/div[1]/article/div[3]/section[2]/div/div/a/span").text)
+
+        # Click on the `likes button`
+        likes_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a[href*='liked_by']")))
+        likes_button.click()
+        time.sleep(self.__TIME_SLEEP)
+        dialogue_box = self._driver.find_element_by_xpath("/html/body/div[5]/div/div/div[2]/div")
+
+        # Get the likes list
+        likes = []
+        for _ in range(0, likes_count // 6):
+            html_list = self._driver.find_element_by_xpath("/html/body/div[5]/div/div/div[2]/div/div")
+            elements_count = len(html_list.find_elements_by_xpath("./*"))
+            for i in range(1, elements_count + 1):
+                text = html_list.find_element_by_xpath(f"//div[{i}]/div[2]/div[1]/div/span/a").get_attribute('title')
+                likes.append(text)
+            self._driver.execute_script("arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;",
+                                        dialogue_box)
+            time.sleep(self.__TIME_SLEEP)
+        return list(set(likes))
+
+    def end(self) -> None:
+        """
+            **Description:** Ends the `webdriver` session
+
+            :returns: None
+            :rtype: None
+        """
+        self._driver.close()
+        print("\n" + "-".center(100, "-"))
+        print("Scraper finished work!".center(100, " "))
+        print(f"Program worked {time.time() - self._start} seconds".center(100, " "))
+        print("-".center(100, "-"))
+
+
+if __name__ == "__main__":
+    scraper = Scraper(__USERNAME, __PASSWORD)
+    scraper.authentication()
+
+    likes_list = scraper.get_likes_list(post_link)
+    pprint({len(likes_list): likes_list})
+
+    follower_list = scraper.get_follower_list(profile_name)
+    pprint({len(follower_list): follower_list})
+
+    scraper.end()
